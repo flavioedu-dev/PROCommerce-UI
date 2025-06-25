77899032ef62f7b03a6dd2cb22739c4b164c896a
Flávio Eduardo <xflax00@gmail.com>
2025-06-24 22:13:47 -0300

    chore: Adiciona novas 'alias'

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
   resolve: {
     alias: {
       '@constants': path.resolve(__dirname, 'src/constants'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@services': path.resolve(__dirname, 'src/services'),
     },
   },
 });
