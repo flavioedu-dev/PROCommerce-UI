69b4b55f0ad1f449476bbcb9279c820cad09cc35
Flávio Eduardo <xflax00@gmail.com>
2025-07-04 20:42:11 -0300

    feat: Implementa página inicial

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from 'react-router';
 import { App } from '../App';
 import { Login } from '@features/auth/login';
 import { Register } from '@features/auth/register/pages/Register';
+import { Homepage } from '@features/homepage/pages/Homepage';
 
 export const router = createBrowserRouter([
   { path: '/', element: <App /> },
   { path: '/login', element: <Login /> },
   { path: '/register', element: <Register /> },
+  { path: '/inicio', element: <Homepage /> },
 ]);
